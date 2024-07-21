22a21e8f75cebba4498e1c69ff42beafd227fd73
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2024-07-21 17:00:26 -0500

    Se agrega interfaz para venta

--- src/app/services/venta.service.ts
+++ src/app/services/venta.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Venta } from '../models/venta';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VentaService {
+  private apiUrl = 'http://api.url/ventas'; // Reemplaza con la URL de tu API
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getVentas(): Observable<Venta[]> {
+    return this.http.get<Venta[]>(this.apiUrl);
+  }
+
+  addVenta(venta: Venta): Observable<Venta> {
+    return this.http.post<Venta>(this.apiUrl, venta);
+  }
+
+  updateVenta(venta: Venta): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${venta.idVenta}`, venta);
+  }
+
+  deleteVenta(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
--- src/app/venta/venta.component.ts
+++ src/app/venta/venta.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VentaService } from '../services/venta.service';
+import { Venta } from '../models/venta';
 
 @Component({
   selector: 'app-venta',
   templateUrl: './venta.component.html',
   styleUrls: ['./venta.component.css']
 })
-export class VentaComponent {
+export class VentaComponent implements OnInit {
+  ventas: Venta[] = [];
+  selectedVenta: Venta | null = null;
+  newVenta: Venta = { idVenta: 0, fecha: new Date(), total: 0, idUsuario: 0, idCliente: 0 };
 
+  constructor(private ventaService: VentaService) { }
+
+  ngOnInit(): void {
+    this.getVentas();
+  }
+
+  getVentas(): void {
+    this.ventaService.getVentas().subscribe(ventas => this.ventas = ventas);
+  }
+
+  addVenta(): void {
+    this.ventaService.addVenta(this.newVenta).subscribe(venta => {
+      this.ventas.push(venta);
+      this.newVenta = { idVenta: 0, fecha: new Date(), total: 0, idUsuario: 0, idCliente: 0 };
+    });
+  }
+
+  updateVenta(): void {
+    if (this.selectedVenta) {
+      this.ventaService.updateVenta(this.selectedVenta).subscribe(() => {
+        this.selectedVenta = null;
+        this.getVentas();
+      });
+    }
+  }
+
+  deleteVenta(id: number): void {
+    this.ventaService.deleteVenta(id).subscribe(() => {
+      this.ventas = this.ventas.filter(venta => venta.idVenta !== id);
+    });
+  }
+
+  selectVenta(venta: Venta): void {
+    this.selectedVenta = { ...venta };
+  }
 }
