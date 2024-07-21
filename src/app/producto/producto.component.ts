7d01a85851ecc1727c15898837fb495443fed8e4
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2024-07-21 16:48:30 -0500

    Interfaz de producto

--- src/app/producto/producto.component.ts
+++ src/app/producto/producto.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductoService } from '../services/producto.service';
+import { Producto } from '../models/producto';
 
 @Component({
   selector: 'app-producto',
   templateUrl: './producto.component.html',
   styleUrls: ['./producto.component.css']
 })
-export class ProductoComponent {
+export class ProductoComponent implements OnInit {
+  productos: Producto[] = [];
+  selectedProducto: Producto | null = null;
+  newProducto: Producto = { idProducto: 0, nombre: '', descripcion: '', precio: 0, stock: 0 };
 
+  constructor(private productoService: ProductoService) { }
+
+  ngOnInit(): void {
+    this.getProductos();
+  }
+
+  getProductos(): void {
+    this.productoService.getProductos().subscribe(productos => this.productos = productos);
+  }
+
+  addProducto(): void {
+    this.productoService.addProducto(this.newProducto).subscribe(producto => {
+      this.productos.push(producto);
+      this.newProducto = { idProducto: 0, nombre: '', descripcion: '', precio: 0, stock: 0 };
+    });
+  }
+
+  updateProducto(): void {
+    if (this.selectedProducto) {
+      this.productoService.updateProducto(this.selectedProducto).subscribe(() => {
+        this.selectedProducto = null;
+        this.getProductos();
+      });
+    }
+  }
+
+  deleteProducto(id: number): void {
+    this.productoService.deleteProducto(id).subscribe(() => {
+      this.productos = this.productos.filter(producto => producto.idProducto !== id);
+    });
+  }
+
+  selectProducto(producto: Producto): void {
+    this.selectedProducto = { ...producto };
+  }
 }
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Producto } from '../models/producto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
+  private apiUrl = 'http://api.url/productos'; // Reemplaza con la URL de tu API
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProductos(): Observable<Producto[]> {
+    return this.http.get<Producto[]>(this.apiUrl);
+  }
+
+  addProducto(producto: Producto): Observable<Producto> {
+    return this.http.post<Producto>(this.apiUrl, producto);
+  }
+
+  updateProducto(producto: Producto): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${producto.idProducto}`, producto);
+  }
+
+  deleteProducto(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
