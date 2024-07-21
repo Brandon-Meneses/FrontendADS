68bda56eea888002146316fe584b814bcf5cdf5b
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2024-07-21 16:54:56 -0500

    Se configuro interfaz de facturas

--- src/app/factura/factura.component.ts
+++ src/app/factura/factura.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FacturaService } from '../services/factura.service';
+import { Factura } from '../models/factura';
 
 @Component({
   selector: 'app-factura',
   templateUrl: './factura.component.html',
   styleUrls: ['./factura.component.css']
 })
-export class FacturaComponent {
+export class FacturaComponent implements OnInit {
+  facturas: Factura[] = [];
+  selectedFactura: Factura | null = null;
+  newFactura: Factura = { idFactura: 0, fechaEmision: new Date(), idVenta: 0 };
 
+  constructor(private facturaService: FacturaService) { }
+
+  ngOnInit(): void {
+    this.getFacturas();
+  }
+
+  getFacturas(): void {
+    this.facturaService.getFacturas().subscribe(facturas => this.facturas = facturas);
+  }
+
+  addFactura(): void {
+    this.facturaService.addFactura(this.newFactura).subscribe(factura => {
+      this.facturas.push(factura);
+      this.newFactura = { idFactura: 0, fechaEmision: new Date(), idVenta: 0 };
+    });
+  }
+
+  updateFactura(): void {
+    if (this.selectedFactura) {
+      this.facturaService.updateFactura(this.selectedFactura).subscribe(() => {
+        this.selectedFactura = null;
+        this.getFacturas();
+      });
+    }
+  }
+
+  deleteFactura(id: number): void {
+    this.facturaService.deleteFactura(id).subscribe(() => {
+      this.facturas = this.facturas.filter(factura => factura.idFactura !== id);
+    });
+  }
+
+  selectFactura(factura: Factura): void {
+    this.selectedFactura = { ...factura };
+  }
 }
--- src/app/services/factura.service.ts
+++ src/app/services/factura.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Factura } from '../models/factura';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FacturaService {
+  private apiUrl = 'http://api.url/facturas'; // Reemplaza con la URL de tu API
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getFacturas(): Observable<Factura[]> {
+    return this.http.get<Factura[]>(this.apiUrl);
+  }
+
+  addFactura(factura: Factura): Observable<Factura> {
+    return this.http.post<Factura>(this.apiUrl, factura);
+  }
+
+  updateFactura(factura: Factura): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${factura.idFactura}`, factura);
+  }
+
+  deleteFactura(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
