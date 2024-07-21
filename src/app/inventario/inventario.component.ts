52bb36d502eede6ade0deed94f0d8e6a82a83a42
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2024-07-21 16:58:05 -0500

    Interfaz de inventario

--- src/app/inventario/inventario.component.ts
+++ src/app/inventario/inventario.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InventarioService } from '../services/inventario.service';
+import { Inventario } from '../models/inventario';
 
 @Component({
   selector: 'app-inventario',
   templateUrl: './inventario.component.html',
   styleUrls: ['./inventario.component.css']
 })
-export class InventarioComponent {
+export class InventarioComponent implements OnInit {
+  inventarios: Inventario[] = [];
+  selectedInventario: Inventario | null = null;
+  newInventario: Inventario = { idInventario: 0, fechaActualizacion: new Date(), idProducto: 0, cantidad: 0 };
 
+  constructor(private inventarioService: InventarioService) { }
+
+  ngOnInit(): void {
+    this.getInventarios();
+  }
+
+  getInventarios(): void {
+    this.inventarioService.getInventarios().subscribe(inventarios => this.inventarios = inventarios);
+  }
+
+  addInventario(): void {
+    this.inventarioService.addInventario(this.newInventario).subscribe(inventario => {
+      this.inventarios.push(inventario);
+      this.newInventario = { idInventario: 0, fechaActualizacion: new Date(), idProducto: 0, cantidad: 0 };
+    });
+  }
+
+  updateInventario(): void {
+    if (this.selectedInventario) {
+      this.inventarioService.updateInventario(this.selectedInventario).subscribe(() => {
+        this.selectedInventario = null;
+        this.getInventarios();
+      });
+    }
+  }
+
+  deleteInventario(id: number): void {
+    this.inventarioService.deleteInventario(id).subscribe(() => {
+      this.inventarios = this.inventarios.filter(inventario => inventario.idInventario !== id);
+    });
+  }
+
+  selectInventario(inventario: Inventario): void {
+    this.selectedInventario = { ...inventario };
+  }
 }
--- src/app/services/inventario.service.ts
+++ src/app/services/inventario.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Inventario } from '../models/inventario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InventarioService {
+  private apiUrl = 'http://api.url/inventarios'; // Reemplaza con la URL de tu API
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getInventarios(): Observable<Inventario[]> {
+    return this.http.get<Inventario[]>(this.apiUrl);
+  }
+
+  addInventario(inventario: Inventario): Observable<Inventario> {
+    return this.http.post<Inventario>(this.apiUrl, inventario);
+  }
+
+  updateInventario(inventario: Inventario): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${inventario.idInventario}`, inventario);
+  }
+
+  deleteInventario(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
